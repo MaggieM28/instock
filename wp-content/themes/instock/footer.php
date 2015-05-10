3737a81541000df4960d36138ad550b1ab702528
MaggieM28 <maddybaddy1291@Magdalenas-MacBook-Pro-3.local>
2015-05-10 15:58:44 -0700

    Nav Top Expand & styling
    Getting the nav started and initial styling

--- wp-content/themes/instock/footer.php
+++ wp-content/themes/instock/footer.php
@@ -1,8 +1,13 @@
 	<div id="footer">
 		<?php wp_footer(); ?>
 	</div>
-      
+
+
 </div><!-- x content -->
+</div><!-- /content-wrap -->
+</div><!-- x container -->
 </div><!-- x rotator -->
+<script src="<?php bloginfo('template_directory'); ?>/js/classie.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/main.js"></script>
 </body>
 </html>
--- wp-content/themes/instock/header.php
+++ wp-content/themes/instock/header.php
@@ -4,6 +4,7 @@
 	<title><?php if(is_home()) bloginfo('name'); else wp_title(''); ?></title>
 	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/normalize.css" />
 	<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 	<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>
@@ -12,3 +13,19 @@
 <body>
 <div id="rotator">
   <div class="container">
+  	<div class="menu-wrap">
+  		<nav class="menu">
+  			<ul class="nav nav-pills pull-right">
+				<li><a href="">Home</a></li>
+				<li><a href="">Categories</a></li>
+				<li ><a href="">Pricing</a></li>
+				<li ><a href="">Contact</a></li>
+			</ul>	
+  		</nav>
+  	</div>
+	<button class="menu-button" id="open-button"></button>
+<div class="content-wrap">
+	<div class="content">
+
+
+
