e0be32cd503a07439ac1b295a4ccf5a600665fbb
MaggieM28 <maddybaddy1291@Magdalenas-MacBook-Pro-3.local>
2015-05-12 17:38:39 -0700

    hover p tag categories

--- wp-content/themes/instock/page-categories.php
+++ wp-content/themes/instock/page-categories.php
@@ -25,7 +25,7 @@ Template Name: Categories Page
 			<img class="img-responsive" src="https://placeimg.com/200/200/nature">
 			<figcaption>
 					<h2>People</h2>
-					<p>She's into crochet. Like really into it.</p>
+					<p>Pictures of People</p>
 					<a href="<?php bloginfo('url'); ?>/people">View more</a>
 			</figcaption>
 			<figure class="effect-ming">
@@ -38,7 +38,7 @@ Template Name: Categories Page
 			<img class="img-responsive" src="https://placeimg.com/200/200/nature">
 			<figcaption>
 					<h2>Nature</h2>
-					<p>She's into crochet. Like really into it.</p>
+					<p>Pictures of Mother Earth</p>
 					<a href="<?php bloginfo('url'); ?>/nature">View more</a>
 			</figcaption>
 			<figure class="effect-ming">
@@ -51,7 +51,7 @@ Template Name: Categories Page
 			<img class="img-responsive" src="https://placeimg.com/200/200/nature">
 			<figcaption>
 					<h2>Animals</h2>
-					<p>She's into crochet. Like really into it.</p>
+					<p>Pictures of Animals</p>
 					<a href="<?php bloginfo('url'); ?>/animals">View more</a>
 			</figcaption>
 			<figure class="effect-ming">
@@ -64,7 +64,7 @@ Template Name: Categories Page
 			<img class="img-responsive" src="https://placeimg.com/200/200/nature">
 			<figcaption>
 					<h2>Buildings</h2>
-					<p>She's into crochet. Like really into it.</p>
+					<p>Pictures of Architecture</p>
 					<a href="<?php bloginfo('url'); ?>/buildings">View more</a>
 			</figcaption>
 			<figure class="effect-ming">
@@ -77,7 +77,7 @@ Template Name: Categories Page
 			<img class="img-responsive" src="https://placeimg.com/200/200/nature">
 			<figcaption>
 					<h2>Travel</h2>
-					<p>She's into crochet. Like really into it.</p>
+					<p>Places of Places</p>
 					<a href="<?php bloginfo('url'); ?>/travel">View more</a>
 			</figcaption>
 			<figure class="effect-ming">
@@ -90,7 +90,7 @@ Template Name: Categories Page
 			<img class="img-responsive" src="https://placeimg.com/200/200/nature">
 			<figcaption>
 					<h2>Urban</h2>
-					<p>She's into crochet. Like really into it.</p>
+					<p>What's Urban? Not Sure, but here are some pictures of it.</p>
 					<a href="<?php bloginfo('url'); ?>/urban">View more</a>
 			</figcaption>
 			<figure class="effect-ming">
@@ -103,7 +103,7 @@ Template Name: Categories Page
 			<img class="img-responsive" src="https://placeimg.com/200/200/nature">
 			<figcaption>
 					<h2>Auto</h2>
-					<p>She's into crochet. Like really into it.</p>
+					<p>Pictures of Automobiles</p>
 					<a href="<?php bloginfo('url'); ?>/auto">View more</a>
 			</figcaption>
 			<figure class="effect-ming">
@@ -116,7 +116,7 @@ Template Name: Categories Page
 			<img class="img-responsive" src="https://placeimg.com/200/200/nature">
 			<figcaption>
 					<h2>Objects</h2>
-					<p>She's into crochet. Like really into it.</p>
+					<p>Pictures of Objects</p>
 					<a href="<?php bloginfo('url'); ?>/objects">View more</a>
 			</figcaption>
 			<figure class="effect-ming">
